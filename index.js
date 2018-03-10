731482b974539bba46775d32ae83df60af144681
Felix Krause <krausefx@gmail.com>
2018-03-10 18:42:27 -0500

    Initial working version

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var needle = require("needle");
 var app = express();
+app.set("view engine", "ejs");
+// Metadata
+var nomadlistUser = "krausefx";
+// Cache
+var currentCityText = "";
+var currentLat = 0.0;
+var currentLng = 0.0;
+var nextCityText = "";
+var nextCityDate = "";
+// Refresher methods
+function updateNomadListData() {
+    var nomadlistUrl = "https://nomadlist.com/@" + nomadlistUser + ".json";
+    needle.get(nomadlistUrl, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        }
+        else if (response.statusCode == 200) {
+            var parsedNomadListData = JSON.parse(body);
+            var now = parsedNomadListData["location"]["now"];
+            var next = parsedNomadListData["location"]["next"];
+            currentCityText = now["city"] + ", " + now["country"];
+            currentLat = now["latitude"];
+            currentLng = now["longitude"];
+            nextCityText = next["city"] + ", " + next["country"];
+            nextCityDate = next["date_start"];
+        }
+    });
+}
+setInterval(updateNomadListData, 60000);
+updateNomadListData();
+// Web server
 app.get("/", function (req, res) {
-    res.send("Hello World");
+    res.render("pages/index", {
+        currentCityText: currentCityText,
+        nextCityText: nextCityText,
+        nextCityDate: nextCityDate
+    });
 });
 var port = 8080;
 app.listen(port);
--- index.ts
+++ index.ts
@@ -1,8 +1,49 @@
 import * as express from "express";
+var needle = require("needle");
 var app = express();
+app.set("view engine", "ejs");
 
+// Metadata
+let nomadlistUser = "krausefx";
+
+// Cache
+let currentCityText = "";
+let currentLat = 0.0;
+let currentLng = 0.0;
+let nextCityText = "";
+let nextCityDate = "";
+
+// Refresher methods
+function updateNomadListData() {
+  let nomadlistUrl = "https://nomadlist.com/@" + nomadlistUser + ".json";
+
+  needle.get(nomadlistUrl, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode == 200) {
+      let parsedNomadListData = JSON.parse(body);
+      let now = parsedNomadListData["location"]["now"];
+      let next = parsedNomadListData["location"]["next"];
+
+      currentCityText = now["city"] + ", " + now["country"];
+      currentLat = now["latitude"];
+      currentLng = now["longitude"];
+
+      nextCityText = next["city"] + ", " + next["country"];
+      nextCityDate = next["date_start"];
+    }
+  });
+}
+setInterval(updateNomadListData, 60000);
+updateNomadListData();
+
+// Web server
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.render("pages/index", {
+    currentCityText: currentCityText,
+    nextCityText: nextCityText,
+    nextCityDate: nextCityDate
+  });
 });
 
 var port = 8080;
