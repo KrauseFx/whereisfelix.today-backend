93bc37eb8b160644ee9a7ec6bbc5dd4ee1ad5566
Felix Krause <krausefx@gmail.com>
2018-03-11 16:58:02 -0400

    Add current mood to page

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 exports.__esModule = true;
 var express = require("express");
 var needle = require("needle");
+var moment = require("moment");
 var app = express();
 app.set("view engine", "ejs");
 // Metadata
 var nomadlistUser = "krausefx";
+var moodHostUrl = "https://krausefx-mood.herokuapp.com/";
 // Cache
 var currentCityText = "";
 var currentLat = null;
 var currentLng = null;
 var nextCityText = null;
 var nextCityDate = null;
+var currentMoodLevel = null;
+var currentMoodEmoji = null;
+var currentModeRelativeTime = null;
 // Refresher methods
 function updateNomadListData() {
     var nomadlistUrl = "https://nomadlist.com/@" + nomadlistUser + ".json";
@@ -32,14 +37,54 @@ function updateNomadListData() {
         }
     });
 }
+function updateMood() {
+    var moodUrl = moodHostUrl + "current_mood.json";
+    needle.get(moodUrl, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        }
+        else if (response.statusCode == 200) {
+            var parsedBody = JSON.parse(body);
+            switch (parseInt(parsedBody["value"])) {
+                case 5:
+                    currentMoodLevel = "pumped, energized";
+                    currentMoodEmoji = "🤩";
+                    break;
+                case 4:
+                    currentMoodLevel = "happy, excited";
+                    currentMoodEmoji = "😃";
+                    break;
+                case 3:
+                    currentMoodLevel = "good, alright";
+                    currentMoodEmoji = "😎";
+                    break;
+                case 2:
+                    currentMoodLevel = "down, worried";
+                    currentMoodEmoji = "😐";
+                    break;
+                case 1:
+                    currentMoodLevel = "Sad, unhappy";
+                    currentMoodEmoji = "😔";
+                    break;
+                case 0:
+                    currentMoodLevel = "Miserable, nervous";
+                    currentMoodEmoji = "😩";
+                    break;
+            }
+            currentModeRelativeTime = moment(new Date(parsedBody["time"])).fromNow();
+        }
+    });
+}
 function allDataLoaded() {
     if (currentCityText == null || nextCityText == null || nextCityDate == null) {
         return false;
     }
     return true;
 }
-setInterval(updateNomadListData, 60000);
+setInterval(updateNomadListData, 60 * 1000);
+setInterval(updateMood, 60 * 1000);
 updateNomadListData();
+updateMood();
 // Web server
 app.get("/", function (req, res) {
     // Because we're using the free Heroku tier for now
@@ -50,7 +95,10 @@ app.get("/", function (req, res) {
         res.render("pages/index", {
             currentCityText: currentCityText,
             nextCityText: nextCityText,
-            nextCityDate: nextCityDate
+            nextCityDate: nextCityDate,
+            currentMoodLevel: currentMoodLevel,
+            currentMoodEmoji: currentMoodEmoji,
+            currentModeRelativeTime: currentModeRelativeTime
         });
     }
     else {
--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
 var needle = require("needle");
+var moment = require("moment");
+
 var app = express();
 app.set("view engine", "ejs");
 
 // Metadata
 let nomadlistUser = "krausefx";
+let moodHostUrl = "https://krausefx-mood.herokuapp.com/";
 
 // Cache
 let currentCityText = "";
@@ -12,6 +15,9 @@ let currentLat: Number = null;
 let currentLng: Number = null;
 let nextCityText: String = null;
 let nextCityDate: String = null;
+let currentMoodLevel: String = null;
+let currentMoodEmoji: String = null;
+let currentModeRelativeTime: String = null;
 
 // Refresher methods
 function updateNomadListData() {
@@ -36,6 +42,43 @@ function updateNomadListData() {
     }
   });
 }
+function updateMood() {
+  let moodUrl = moodHostUrl + "current_mood.json";
+  needle.get(moodUrl, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode == 200) {
+      let parsedBody = JSON.parse(body);
+      switch (parseInt(parsedBody["value"])) {
+        case 5:
+          currentMoodLevel = "pumped, energized";
+          currentMoodEmoji = "🤩";
+          break;
+        case 4:
+          currentMoodLevel = "happy, excited";
+          currentMoodEmoji = "😃";
+          break;
+        case 3:
+          currentMoodLevel = "good, alright";
+          currentMoodEmoji = "😎";
+          break;
+        case 2:
+          currentMoodLevel = "down, worried";
+          currentMoodEmoji = "😐";
+          break;
+        case 1:
+          currentMoodLevel = "Sad, unhappy";
+          currentMoodEmoji = "😔";
+          break;
+        case 0:
+          currentMoodLevel = "Miserable, nervous";
+          currentMoodEmoji = "😩";
+          break;
+      }
+      currentModeRelativeTime = moment(new Date(parsedBody["time"])).fromNow();
+    }
+  });
+}
 
 function allDataLoaded() {
   if (currentCityText == null || nextCityText == null || nextCityDate == null) {
@@ -44,8 +87,11 @@ function allDataLoaded() {
   return true;
 }
 
-setInterval(updateNomadListData, 60000);
+setInterval(updateNomadListData, 60 * 1000);
+setInterval(updateMood, 60 * 1000);
+
 updateNomadListData();
+updateMood();
 
 // Web server
 app.get("/", function(req, res) {
@@ -57,7 +103,10 @@ app.get("/", function(req, res) {
     res.render("pages/index", {
       currentCityText: currentCityText,
       nextCityText: nextCityText,
-      nextCityDate: nextCityDate
+      nextCityDate: nextCityDate,
+      currentMoodLevel: currentMoodLevel,
+      currentMoodEmoji: currentMoodEmoji,
+      currentModeRelativeTime: currentModeRelativeTime
     });
   } else {
     res.render("pages/loading");
