d6513bb5124636f42ebc69c4e2262f6f8ca8a02b
Felix Krause <krausefx@gmail.com>
2018-04-23 09:50:24 +0200

    Add automate refresh + loading for the backend to launch

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,11 +11,19 @@ import { MyData, DataService } from './data.service';
 
 export class AppComponent {
   title = 'app';
+  refresherInterval = null;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.refreshData();
+
+    // Poor person's reload for now, until we have proper
+    // web streams and stuff
+    let self = this;
+    this.refresherInterval = setInterval(function() {
+      self.refreshData();
+    }, 3000);
   }
 
   data: MyData;
@@ -23,8 +31,20 @@ export class AppComponent {
   refreshData() {
     this.dataService.getData()
       .subscribe(data => {
-        console.log(data);
         this.data = { ...data };
+        console.log(this.data)
+
+        if (this.data.loading != true) {
+          // We got data, let's stop refreshing now
+          clearInterval(this.refresherInterval);
+        }
+      },
+      error => {
+        console.log("Something went wrong...")
+        console.log(error)
+        this.data = <MyData> {
+          loading: true
+        }
       })
   }
 }
